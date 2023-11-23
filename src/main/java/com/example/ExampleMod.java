d8170d8f8cfe375d23c6eee75cbc72cacc00056d
Plat00n <70469468+DefinitlyNotABot@users.noreply.github.com>
2023-11-23 18:08:24 +0100

    Settings avaliable

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.fabricmc.api.ModInitializer;
-
-
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
@@ -14,22 +17,19 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.border.WorldBorder;
+import org.apache.commons.lang3.EnumUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static net.minecraft.server.command.CommandManager.*;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 
 public class ExampleMod implements ModInitializer {
@@ -40,11 +40,12 @@ public class ExampleMod implements ModInitializer {
 
 	private static final int ModMayorVersion = 1;
 	private static final int ModMidVersion = 0;
-	private static final int ModMinorVersion = 5;
+	private static final int ModMinorVersion = 6;
 
 	private enum settings {
 		DUFFUCULTY_LEVEL("difficulty_level"),
 		MAX_USES_PER_ITEM("max_uses_per_item"),
+		INCREASE_PER_PAY("increase_per_pay"),
 		MOD_MAYOR_VERSION("mod_mayor_version"),
 		MOD_MID_VERSION("mod_mid_version"),
 		MOD_MINOR_VERSION("mod_minor_version"),
@@ -82,6 +83,17 @@ public class ExampleMod implements ModInitializer {
 		}
 	}
 
+	private String[] keys = new String[]{
+			settings.DUFFUCULTY_LEVEL.toString(),
+			settings.MAX_USES_PER_ITEM.toString(),
+			settings.INCREASE_PER_PAY.toString()
+	};
+	private int[] standardValues = new int[]{
+			2000,
+			10,
+			1000
+	};
+
 	@Override
 	public void onInitialize() {
 
@@ -111,8 +123,33 @@ public class ExampleMod implements ModInitializer {
 			);
 		});
 
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(
+					literal("change_settings")
+							.then(argument("setting", StringArgumentType.word())
+									.suggests((context, builder) -> suggestions_settings(builder))
+									.then(argument("value", DoubleArgumentType.doubleArg())
+											.executes(this::change_settings)
+									)
+							)
+			);
+		});
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(
+					literal("get_settings")
+							.then(argument("setting", StringArgumentType.word())
+									.suggests((context, builder) -> suggestions_settings(builder))
+											.executes(this::get_settings)
+
+							)
+			);
+		});
+
 	}
 
+	//region Player callable Functions
 	private int payborder(CommandContext<ServerCommandSource> context) {
 
 		ServerCommandSource source = context.getSource();
@@ -156,8 +193,8 @@ public class ExampleMod implements ModInitializer {
 			context.getSource().sendMessage(Text.literal(errorMessage(-3)));
 			return -1;
 		}
-
-		int price = (int) Math.pow(dictionarySettings.get(settings.DUFFUCULTY_LEVEL.toString()), count);
+		double value = dictionarySettings.get(settings.DUFFUCULTY_LEVEL.toString());
+		int price = (int) Math.pow(value/1000, count);
 
 		if (price > itemCount) {
 			context.getSource().sendMessage(Text.literal(errorMessage(-1)));
@@ -184,7 +221,7 @@ public class ExampleMod implements ModInitializer {
 
 		ServerWorld serverWorld = player.getServer().getOverworld();
 		WorldBorder worldBorder = serverWorld.getWorldBorder();
-		double newSize = worldBorder.getSize() + 1.0;
+		double newSize = worldBorder.getSize() + dictionarySettings.get(settings.INCREASE_PER_PAY.toString())/1000f;
 		worldBorder.setSize(newSize);
 
 		List<ServerPlayerEntity> playerList = context.getSource().getServer().getPlayerManager().getPlayerList();
@@ -192,7 +229,7 @@ public class ExampleMod implements ModInitializer {
 		String finalItemName = itemName;
 		String playerName = player.getEntityName();
 		playerList.forEach(player2 -> player2.sendMessage(Text.literal(playerName + " sold " + price + "x " + finalItemName), false));
-		playerList.forEach(player2 -> player2.sendMessage(Text.literal("The worldborder has been increased to: " + newSize), false));
+		playerList.forEach(player2 -> player2.sendMessage(Text.literal("The world border has been increased to: " + newSize), false));
 
 
 		return 0;
@@ -235,7 +272,8 @@ public class ExampleMod implements ModInitializer {
 			return -1;
 		}
 
-		price = (int) Math.pow(dictionarySettings.get(settings.DUFFUCULTY_LEVEL.toString()), count);
+		double value = dictionarySettings.get(settings.DUFFUCULTY_LEVEL.toString());
+		price = (int) Math.pow(value/1000, count);
 
 
 		if(price < 0)
@@ -282,8 +320,9 @@ public class ExampleMod implements ModInitializer {
 		dictionary.put(settings.MOD_MAYOR_VERSION.toString(), ModMayorVersion);
 		dictionary.put(settings.MOD_MID_VERSION.toString(), ModMidVersion);
 		dictionary.put(settings.MOD_MINOR_VERSION.toString(), ModMinorVersion);
-		dictionary.put(settings.DUFFUCULTY_LEVEL.toString(), 2);
+		dictionary.put(settings.DUFFUCULTY_LEVEL.toString(), 2000);
 		dictionary.put(settings.MAX_USES_PER_ITEM.toString(), 10);
+		dictionary.put(settings.INCREASE_PER_PAY.toString(), 1000);
 
 
 		createFile(settingsFilePath);
@@ -332,7 +371,7 @@ public class ExampleMod implements ModInitializer {
 			writeFile(blockFilePath, dictionary);
 
 			String settingsFilePath = filepath.SETTINGS.getFile(world);
-			dictionary.put(settings.DUFFUCULTY_LEVEL.toString(), 2);
+			dictionary.put(settings.DUFFUCULTY_LEVEL.toString(), 2000);
 			dictionary.put(settings.MAX_USES_PER_ITEM.toString(), 10);
 
 			createFile(settingsFilePath);
@@ -364,10 +403,24 @@ public class ExampleMod implements ModInitializer {
 			return -1;
 		}
 
+		if(compareVersions(dictionarySettings, context) <= 0){
+			context.getSource().sendMessage(Text.literal("The version won't be changed"));
+			return 0;
+		}
+
+		if(dictionarySettings.get(settings.MOD_MAYOR_VERSION.toString()) == 1 && dictionarySettings.get(settings.MOD_MID_VERSION.toString()) == 0 && dictionarySettings.get(settings.MOD_MINOR_VERSION.toString()) < 6){
+			dictionarySettings.put(settings.DUFFUCULTY_LEVEL.toString(), dictionarySettings.get(settings.DUFFUCULTY_LEVEL.toString())*1000);
+		}
 
 		dictionarySettings.put(settings.MOD_MAYOR_VERSION.toString(), ModMayorVersion);
 		dictionarySettings.put(settings.MOD_MID_VERSION.toString(), ModMidVersion);
 		dictionarySettings.put(settings.MOD_MINOR_VERSION.toString(), ModMinorVersion);
+
+		for(int i = 0; i < keys.length; i++){
+			if(!dictionarySettings.containsKey(keys[i])){
+				dictionarySettings.put(keys[i], standardValues[i]);
+			}
+		}
 
 		writeFile(settingsFilePath, dictionarySettings);
 
@@ -376,41 +429,144 @@ public class ExampleMod implements ModInitializer {
 		return 0;
 	}
 
-	private void compareVersions(Map<String, Integer> dictionarySettings, CommandContext<ServerCommandSource> context)
-	{
+	private int change_settings(CommandContext<ServerCommandSource> context){
+
+		String setting = StringArgumentType.getString(context, "setting");
+		boolean valid = false;
+		for (settings settingCompare : settings.values()) {
+			if(settingCompare.equals(setting)){
+				valid = true;
+			}
+		}
+		if(!valid){
+			context.getSource().sendMessage(Text.literal("Not a valid setting"));
+			return -1;
+		}
+		double value = DoubleArgumentType.getDouble(context, "value");
+
+		ServerCommandSource source = context.getSource();
+		ServerPlayerEntity player = source.getPlayer();
+		assert player != null;
+
+		String world = Objects.requireNonNull(player.getServer()).getOverworld().toString();
+		world = world.substring(world.indexOf("[") + 1);
+		world = world.substring(0, world.indexOf("]"));
+
+
+		String settingsFilePath = filepath.SETTINGS.getFile(world);
+		Map<String, Integer> dictionarySettings = ReadFile(settingsFilePath);
+
+		if(compareVersions(dictionarySettings, context) != 0){
+			context.getSource().sendMessage(Text.literal("Incompatible version"));
+			return -1;
+		}
+		dictionarySettings.put(setting, (int)(value*1000));
+		writeFile(settingsFilePath, dictionarySettings);
+
+		context.getSource().sendMessage(Text.literal("Done"));
+
+		return 0;
+	}
+
+	private int get_settings(CommandContext<ServerCommandSource> context){
+
+		String setting = StringArgumentType.getString(context, "setting");
+		boolean valid = false;
+		for (settings settingCompare : settings.values()) {
+			if(settingCompare.equals(setting)){
+				valid = true;
+			}
+		}
+		if(!valid){
+			context.getSource().sendMessage(Text.literal("Not a valid setting"));
+			return -1;
+		}
+
+		ServerCommandSource source = context.getSource();
+		ServerPlayerEntity player = source.getPlayer();
+		assert player != null;
+
+		String world = Objects.requireNonNull(player.getServer()).getOverworld().toString();
+		world = world.substring(world.indexOf("[") + 1);
+		world = world.substring(0, world.indexOf("]"));
+
+
+		String settingsFilePath = filepath.SETTINGS.getFile(world);
+		Map<String, Integer> dictionarySettings = ReadFile(settingsFilePath);
+		assert dictionarySettings != null;
+		if(settings.DUFFUCULTY_LEVEL.equals(setting)){
+            context.getSource().sendMessage(Text.literal("" + dictionarySettings.get(setting)/1000f));
+		}
+		if(settings.INCREASE_PER_PAY.equals(setting)){
+			context.getSource().sendMessage(Text.literal("" + dictionarySettings.get(setting)/1000f));
+		}
+		if(settings.MAX_USES_PER_ITEM.equals(setting)){
+			context.getSource().sendMessage(Text.literal("" + dictionarySettings.get(setting)));
+		}
+
+		return 0;
+	}
+
+
+	//endregion
+
+
+
+	//region Helper Functions
+
+	private CompletableFuture<Suggestions> suggestions_settings(SuggestionsBuilder builder) {
+		List<settings> ignoreSettings = new ArrayList<settings>();
+		ignoreSettings.add(settings.MOD_MAYOR_VERSION);
+		ignoreSettings.add(settings.MOD_MID_VERSION);
+		ignoreSettings.add(settings.MOD_MINOR_VERSION);
+
+		for (settings setting : settings.values()) {
+			if(ignoreSettings.contains(setting)){
+				continue;
+			}
+			builder.suggest(setting.toString());
+		}
+
+		return builder.buildFuture();
+	}
+
+
+	private int compareVersions(Map<String, Integer> dictionarySettings, CommandContext<ServerCommandSource> context) {
 		String newVer = "Your version is newer than the servers";
 		String oldVer = "Your mod is outdated";
 		if(!dictionarySettings.containsKey(settings.MOD_MAYOR_VERSION.toString())){
 			context.getSource().sendMessage(Text.literal(newVer));
+			return 1;
 		}
 		if(dictionarySettings.get(settings.MOD_MAYOR_VERSION.toString()) < ModMayorVersion)
 		{
 			context.getSource().sendMessage(Text.literal(newVer));
+			return 1;
 		}else if(dictionarySettings.get(settings.MOD_MAYOR_VERSION.toString()) < ModMayorVersion){
 			context.getSource().sendMessage(Text.literal(oldVer));
+			return -1;
 		}
 
 		if(dictionarySettings.get(settings.MOD_MID_VERSION.toString()) < ModMidVersion)
 		{
 			context.getSource().sendMessage(Text.literal(newVer));
+			return 1;
 		}else if(dictionarySettings.get(settings.MOD_MID_VERSION.toString()) < ModMidVersion){
 			context.getSource().sendMessage(Text.literal(oldVer));
+			return -1;
 		}
 		if(dictionarySettings.get(settings.MOD_MINOR_VERSION.toString()) < ModMinorVersion)
 		{
 			context.getSource().sendMessage(Text.literal(newVer));
+			return 1;
 		}else if(dictionarySettings.get(settings.MOD_MINOR_VERSION.toString()) < ModMinorVersion){
 			context.getSource().sendMessage(Text.literal(oldVer));
+			return -1;
 		}
-
+		return 0;
 	}
 
-
-
-
-
-	private @Nullable Map<String, Integer> ReadFile(String filePath)
-	{
+	private @Nullable Map<String, Integer> ReadFile(String filePath) {
 		try{
 			Map<String, Integer> dictionary = new HashMap<>();
 			System.out.println("File: " + filePath);
@@ -430,8 +586,7 @@ public class ExampleMod implements ModInitializer {
 		}
 	}
 
-	private void writeFile(String filePath, @NotNull Map<String, Integer> dictionary)
-	{
+	private void writeFile(String filePath, @NotNull Map<String, Integer> dictionary) {
 
 		try (FileWriter writer = new FileWriter(filePath, false)) {
 			// Write header (if needed)
@@ -467,9 +622,7 @@ public class ExampleMod implements ModInitializer {
 
 	}
 
-
-	private String errorMessage(int errorCode)
-	{
+	private String errorMessage(int errorCode) {
         return switch (errorCode) {
             case -1 -> "Too few Items";
             case -2 -> "ERROR -> Could not find file";
@@ -478,4 +631,5 @@ public class ExampleMod implements ModInitializer {
         };
     }
 
+	//endregion
 }
